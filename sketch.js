02aaaa32b624ab1d4f85c736b7522d722750a87a
SohamJain789 <69146549+SohamJain789@users.noreply.github.com>
2020-11-06 23:25:48 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,15 +2,18 @@ var president,presidentImg,buildings,buildingImg,soldierAmt=0;
 var buildingGroup1,buildingGroup2,buildingGroup3,buildingGroup4
 var time =1;
 var gameState=1;
-var soldier,soldierImg,tank,tankImg;
-var money = 10000,tankAmt=0;
+var soldier,soldierImg,tank,tankImg,helicopter,helicopterImg;
+var money = 10000,tankAmt=0,helicopterAmt=0;
+var shield,shieldImg,shieldAmt=0;
+var bigShieldAmt=0,aliens=25000;
 function preload() {
   // body...
   presidentImg = loadImage("images/presidentImg.png");
   buildingImg = loadImage("images/building.png");
   soldierImg = loadImage("images/soldier.png");
   tankImg = loadImage("images/tank.png");
-
+  helicopterImg = loadImage("images/helicopter.png");
+  shieldImg = loadImage("images/shield.png");
 }
 
 function setup(){
@@ -25,7 +28,7 @@ function setup(){
 
 function draw(){
 background(0,0,0);
-time=time+1;
+
 fill("white");
 textSize(15);
 text("Money:"+money,0,100)
@@ -78,17 +81,33 @@ if (gameState==2) {
  }
  
  if (gameState==5) {
-  text("Hello and welcome to the shop There are many items that you might purchase",300,100);
+  text("Hello and welcome to the shop There are many items that you might purchase.Spend your money wisely",300,100);
+  fill("red")
+  text("Press z when you are done",300,140);
+  fill("white")
   soldier = createSprite(200,200,30,30);
   soldier.addImage("soldier",soldierImg);
   soldier.scale = 0.3;
   president.visible = false;
-  tank = createSprite(400,200,30,30);
+  tank = createSprite(450,200,30,30);
   tank.addImage("tank",tankImg);
-  tank.scale = 0.3;
+  tank.scale = 0.5;
+  helicopter = createSprite(750,200,30,30);
+  helicopter.addImage("helicopter",helicopterImg);
+  helicopter.scale = 0.5;
+  shield = createSprite(1150,200,30,30);
+  shield.addImage("shield",shieldImg);
+  shield.scale = 0.3;
   text("This is a tank for 100 coin.Press t to buy it",350,350)
-
   text("This is a soldier for 1 coin.Press s to buy it",50,350)
+  text("This is a helicopter for 200 coin.Press h to buy it",650,350)
+  text("This is a shield for 50 coin(Only buyable 5 times).Press d to buy it",1050,350)
+  //text("A scientist in your country offered you a big shield this is a one time offer and you can only buy 1 big shield by pressing b for 250 coins",350,450);
+  text("helicopter damage : 10000",50,550);
+  text("soldier damage : 10",50,570);
+  text("tank damage : 5000",50,590);
+  text("shield protection : damage reduced by 35%",50,610);
+  //text("Big shield damage reduction : 60%",50,630);
   if (keyDown("s")) {
   soldierAmt=soldierAmt + 1;
   money=money-1;
@@ -99,6 +118,62 @@ if (gameState==2) {
   money=money-100;
  }
  text("Tanks:"+tankAmt,20,230)
+
+  text("Soldiers:"+soldierAmt,20,200)
+ if (keyDown("h")) {
+  helicopterAmt=helicopterAmt + 1;
+  money=money-200;
+ }
+ text("Helicopter:"+helicopterAmt,20,250);
+ if (keyDown("d") && shieldAmt!==5) {
+  shieldAmt=shieldAmt + 1;
+  money=money-50;
+ }
+ text("Shield:"+shieldAmt,20,270);
+  /*if (keyDown("b") && bigShieldAmt==0) {
+   bigShieldAmt=bigShieldAmt + 1;
+   money=money-250;
+ }
+ text("BigShield:"+bigShieldAmt,20,290);*/
+ if (keyDown("z")) {
+  gameState = 6;
+ }
+  helicopter.visible = false;
+  soldier.visible = false;
+  tank.visible = false;
+  shield.visible = false;
+ }
+ if (gameState==6) {
+  text("The aliens are now attacking.The soldiers are placed on the border of your country",150,100);
+  //text("Click the mouse to place the soldiers.The helicopters and shield are placed by default",150,130);
+  time=time+1;
+  
+    if(time>520){
+      soldierAmt=soldierAmt-[5/{shieldAmt + 5}];
+      helicopterAmt=helicopterAmt-[5/{shieldAmt + 5}];
+      tankAmt=tankAmt-[5/{shieldAmt+5}];
+      aliens=aliens-(soldierAmt/100);
+      aliens=aliens-(tankAmt/50);
+      aliens=aliens-(helicopterAmt/25);
+      if (aliens===0) {
+          gameState=7;
+        }   else if (soldierAmt==0 && tankAmt==0 && helicopterAmt==0) {
+          gameState=8;
+        }
+    }  
+  row1();
+  row2();
+  row3();
+  row4();
+
+
+ }
+ if (gameState=7) {
+  text("YOU LOSE TRY AGAIN BY PRESSING CTRL+R",100,300);
+ }
+ if (gameState=8) {
+  text("YOU WIN PLAY AGAIN BY PRESSING CTRL+R",100,300);
+
  }
 
 drawSprites()
@@ -133,7 +208,4 @@ function row3(){
    buildings.scale = 0.5;
    buildingGroup4.add(buildings);
  }
-}
-function mousePressed(){
-  
 }
